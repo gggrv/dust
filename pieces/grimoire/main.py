307bf3f402c0a23472552f0c90792855f2edf7bc
gggrv <43758914+gggrv@users.noreply.github.com>
2021-04-02 19:57:40 +0200

    added file drag and drop support

--- pieces/grimoire/main.py
+++ pieces/grimoire/main.py
@@ -12,7 +12,59 @@ import pyautogui
 from PyQt5 import QtCore, QtGui, QtWidgets
 # same folder
 
-class followindow( QtWidgets.QDialog ):
+class hungrywidget( QtWidgets.QWidget ):
+    
+    # minimum height of the open mouth
+    zev_height = 100
+    
+    def __init__( self,
+                  parent=None,
+                  *args, **kwargs ):
+        super( hungrywidget, self ).__init__( parent, *args, **kwargs )
+        
+        # gui
+        self._init()
+        self._init_staticwidgets()
+        
+    """---------------------------------------------------------------------+++
+    Everything about init.
+    """
+    def _init( self ):
+        self.setObjectName( 'hungrywidget' )
+        
+    def _init_staticwidgets( self ):
+        
+        # layout
+        lyt = QtWidgets.QVBoxLayout()
+        lyt.setObjectName( 'vbox' )
+        lyt.setContentsMargins(0,0,0,0)
+        
+        # top label
+        eyes = QtWidgets.QLabel( self )
+        eyes.setObjectName( 'eyes' )
+        eyes.setText( '     Θ     Θ     ' )
+        jaw1 = QtWidgets.QLabel( self )
+        jaw1.setObjectName( 'jaw1' )
+        jaw1.setText( 'VVVVVVVVVVVVVVVVV' )
+        
+        # middle label
+        zev = QtWidgets.QLabel( self )
+        zev.setObjectName( 'zev' )
+        zev.setMinimumHeight( self.zev_height )
+        
+        # bottom label
+        jaw2 = QtWidgets.QLabel( self )
+        jaw2.setObjectName( 'jaw2' )
+        jaw2.setText( 'ЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛ' )
+        
+        # assemble
+        lyt.addWidget( eyes )
+        lyt.addWidget( jaw1 )
+        lyt.addWidget( zev )
+        lyt.addWidget( jaw2 )
+        self.setLayout( lyt )
+
+class followindow( QtWidgets.QWidget ):
     
     # distance from mouse
     mouse_distance = [50,50] # where to stop near the mouse
@@ -28,8 +80,20 @@ class followindow( QtWidgets.QDialog ):
         super( followindow, self ).__init__( parent, *args, **kwargs )
         
         # gui
-        self._init()
         self._init_staticwidgets()
+        
+        
+        # i *must* call the following lines from the constructor,
+        # because otherwise dropping items with mouse on the
+        # followindow does not work.
+        # this behaviour heavily depends on the window flag
+        # that i *need*: QtCore.Qt.SplashScreen
+        self.setWindowFlags(
+            QtCore.Qt.FramelessWindowHint
+            | QtCore.Qt.WindowStaysOnTopHint
+            | QtCore.Qt.SplashScreen
+            ) # !!!
+        self.setAcceptDrops(True) # !!!
         
     """---------------------------------------------------------------------+++
     Everything about events.
@@ -41,31 +105,62 @@ class followindow( QtWidgets.QDialog ):
         self.destroy()
         ev.ignore() # !!!
         
-    def mouseEnter( self, ev ):
+    def dragEnterEvent( self, ev ):
+        print( 'enter', ev.mimeData() )
+        
+        # show the hungry widget
+        self.hung.setVisible(True)
+        
         # show enlarged window, mouse in the middle
-        # current dims and coords
-        cx,cy = self.x(), self.y()
-        
-        # next dims and coords
-        nw,nh = self.bigger_dims
-        
-        # apply
+        cx,cy = self.x(), self.y() # current dims and coords
+        nw,nh = self.hung.width(),self.hung.height() # next dims and coords
         self.resize( nw,nh )
         self.move( cx-nw//2, cy-nh//2 )
         
-        # keyboard focus
-        self.setFocus()
+        #if ev.mimeData().hasText(): ev.accept()
+        #else: ev.ignore()
+        ev.accept()
+        
+    def dropEvent( self, ev ):
+        print( 'drop' )
+        
+        # hide the hungry widget
+        self.hung.setVisible(False)
+        
+        # show small window near the mouse
+        cw,ch = self.width(),self.height() # current dims and coords↓
+        cx,cy = self.x(), self.y()
+        nw,nh = self.smaller_dims # next dims and coords
+        self.resize( nw,nh )
+        self.move( cx+cw//2, cy+ch//2 )
+        
+        ev.accept()
+        
+    def dragLeaveEvent( self, ev ):
+        # hide the hungry widget
+        self.hung.setVisible(False)
+        
+        # show small window near the mouse
+        cw,ch = self.width(),self.height() # current dims and coords↓
+        cx,cy = self.x(), self.y()
+        nw,nh = self.smaller_dims # next dims and coords
+        self.resize( nw,nh )
+        self.move( cx+cw//2, cy+ch//2 )
+        
+        ev.accept()
+        
+    def mouseEnter( self, ev ):
+        # show enlarged window, mouse in the middle
+        cx,cy = self.x(), self.y() # current dims and coords
+        nw,nh = self.bigger_dims # next dims and coords
+        self.resize( nw,nh )
+        self.move( cx-nw//2, cy-nh//2 )
         
     def mouseLeave( self, ev ):
         # show small window near the mouse
-        # current dims and coords
-        cw,ch = self.width(),self.height()
+        cw,ch = self.width(),self.height() # current dims and coords↓
         cx,cy = self.x(), self.y()
-        
-        # next dims and coords
-        nw,nh = self.smaller_dims
-        
-        # apply
+        nw,nh = self.smaller_dims # next dims and coords
         self.resize( nw,nh )
         self.move( cx+cw//2, cy+ch//2 )
         
@@ -93,17 +188,6 @@ class followindow( QtWidgets.QDialog ):
     """---------------------------------------------------------------------+++
     Everything about init.
     """
-    def _init( self ):
-        self.setWindowFlags(
-            QtCore.Qt.FramelessWindowHint
-            | QtCore.Qt.WindowStaysOnTopHint
-            | QtCore.Qt.SplashScreen
-            )
-        
-        # i want to access mouse and keyboard input
-        #self.setMouseTracking( True )
-        self.setFocusPolicy( QtCore.Qt.StrongFocus )
-        
     def _init_staticwidgets( self ):
         
         # layout
@@ -111,14 +195,25 @@ class followindow( QtWidgets.QDialog ):
         lyt.setObjectName( 'vbox' )
         lyt.setContentsMargins(0,0,0,0)
         
-        # label
-        lab = QtWidgets.QWidget( self )
-        lab.setObjectName( 'bg' )
-        lab.setStyleSheet( 'background-color:rgb(33,33,33);' )
+        # background label
+        bg = QtWidgets.QWidget( self )
+        bg.setObjectName( 'bg' )
+        bg.setStyleSheet( 'background-color:rgb(33,33,33);color:white;font-family:consolas' )
         
-        # assemble
-        lyt.addWidget( lab )
+        # hidden hungry widget
+        self.hung = hungrywidget( bg )
+        self.hung.setVisible( False )
+        
+        # assemble self
+        lyt.addWidget( bg )
         self.setLayout( lyt )
+        
+        # i *must* call self.show, because if i use .show() from another
+        # object, dropping items with mouse on the
+        # followindow does not work.
+        # this behaviour heavily depends on the window flag
+        # that i *need*: QtCore.Qt.SplashScreen
+        self.show() # !!!
 
 class interaction_object( QtWidgets.QMainWindow ):
     
@@ -141,6 +236,7 @@ class interaction_object( QtWidgets.QMainWindow ):
     """---------------------------------------------------------------------+++
     Everything about followindows.
     """
+    
     def eventFilter( self, ob, ev ):
         # help:
         # https://stackoverflow.com/questions/52291734/pyqt5-mouse-hover-functions
@@ -156,8 +252,6 @@ class interaction_object( QtWidgets.QMainWindow ):
             elif ev.type()==11:
                 self.follotimer.start()
                 ob.mouseLeave(ev)
-        if ev.type()==QtCore.QEvent.MouseMove:
-            print('SDFGHJU')
             
         return super(interaction_object,self).eventFilter( ob, ev )
     
@@ -238,7 +332,8 @@ class interaction_object( QtWidgets.QMainWindow ):
         
         for iloc in range(1):
             # spawn the window
-            w = followindow( parent=self )
+            #w = followindow( parent=self )
+            w = followindow()
             
             # give it unique object name
             w.setObjectName( 'followindow_%s'%iloc )
@@ -288,4 +383,4 @@ class interaction_object( QtWidgets.QMainWindow ):
         return actions
     
 #---------------------------------------------------------------------------+++
-# конец 2021.03.30 → 2021.04.01
+# конец 2021.03.30 → 2021.04.02
